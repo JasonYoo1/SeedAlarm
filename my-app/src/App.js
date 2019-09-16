0281e098bd4655e9433228333b6c331d2a0735cf
jason-yoo <jasonyoo@Jasons-MacBook-Air.local>
2019-09-16 15:43:06 -0700

    added checkout tab

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,7 @@ import Menu from "./components/menu";
 import Dashboard from "./components/Dashboard";
 import Registration from './components/Register'
 import SearchInfo from './components/Search'
+import CheckOut from './components/Checkout'
 
 function App() {
   // const [creds, setCreds] = useState([]);
@@ -46,6 +47,7 @@ function App() {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/activate" component={Registration} />
             <Route exact path ="/search" component={SearchInfo}/>
+            <Route exact path ="/checkout" component={CheckOut}/>
           </Switch>
           <Footer />
         </div>
