6d425e9c8ac68341525e8b03be28130e01bf52e5
jason-yoo <jasonyoo@Jasons-MacBook-Air.local>
2019-09-18 15:34:23 -0700

    something chnanged and it works

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axiosWithAuth from "../Utils/axiosWithAuth.js.js";
+import axiosWithAuth from "../Utils/axiosWithAuth";
 import axios from 'axios'
 
 import { Link } from "react-router-dom";
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { PayPalButton } from "react-paypal-button-v2";
 
 //styles
 import "bootstrap/dist/css/bootstrap.min.css";
