56e6b6e7709f053f408dd60f698ef8c446cee067
jason-yoo <jyoojs@gmail.com>
2019-10-01 14:55:47 -0700

    made changes to map

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -13,7 +13,9 @@ class SimpleMap extends Component {
  
   render() {
     return (
-      <embed src="https://maps.t-mobile.com/pcc.html?map=mvno-noroam-34l"style={{width:'1000px', height: "650px"}} ></embed>
+      <div className='tmobilemap'>
+      <embed src="https://maps.t-mobile.com/pcc.html?map=mvno-noroam-34l"style={{width:'90%', height: "650px"}} ></embed>
+      </div>
     );
   }
 }
