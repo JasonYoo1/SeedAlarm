4ae79637bdf1a80dedede01cf03ccf199878b1ed
jason-yoo <jyoojs@gmail.com>
2019-10-07 13:05:36 -0700

    made changes to 0.01

--- src/components/RefillCheckOut.js
+++ src/components/RefillCheckOut.js
@@ -74,11 +74,18 @@ const RefillCheckOut = ({ history }) => {
             <h3 className= 'paraText'>$ 14.99</h3>
            </Card>
            <div onClick={handleClick} id='myBtn' className = 'checkoutPay'>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            {/* <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick"/>
             <input type="hidden" name="hosted_button_id" value="VS7Q35U928MNL"/>
             <input type="image" width="100%" src={paypalbtn} border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
+            </form> */}
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick"/>
+            <input type="hidden" name="hosted_button_id" value="EFM3KRU92KHDN"/>
+            <input type="image" width="100%" src={paypalbtn} border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
             </form>
+
             </div>
             </Row>
             </FormGroup>
@@ -98,11 +105,18 @@ const RefillCheckOut = ({ history }) => {
             <h3 className= 'paraText'>$ 5.99</h3>
            </Card>
            <div onClick={handleClick} className = 'checkoutPay'>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            {/* <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick"/>
             <input type="hidden" name="hosted_button_id" value="WQQNXR6L28RSS"/>
             <input type="image" width="100%" src={paypalbtn} border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
+            </form> */}
+
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick"/>
+            <input type="hidden" name="hosted_button_id" value="EFM3KRU92KHDN"/>
+            <input type="image" width="100%" src={paypalbtn} border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
             </form>
+
                </div>
 
             </Row>
